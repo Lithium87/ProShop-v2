0c49ff577c5a1a8c931f4307399ae2e4a690bf33
Lithium@87 <cibs@abv.bg>
2025-01-06 19:02:40 +0200

    Create database and connect it to application

--- .history/backend/server_20250106190003.js
+++ .history/backend/server_20250106190003.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config ();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
-const port = 5000;
+connectDB (); // Connect to MongoDB
+
+const port = process.env.PORT || 5000;
 
 const app = express ();
 
@@ -14,7 +19,7 @@ app.get ('/api/products', (req, res) => {
 });
 
 app.get ('/api/products/:id', (req, res) => {
-  const product = products.find (p => p._id === req.params.id);
+  const product = products.find (p => p._id === +req.params.id);
   res.json (product);
 });
 
