e66a63226f7a26a4d841d99a83b1ee6b6bea1c54
Lithium@87 <cibs@abv.bg>
2025-01-10 20:11:08 +0200

    Create product routes

--- .history/backend/server_20250110194434.js
+++ .history/backend/server_20250110194434.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config ();
 import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
+import products from './data/products.js';
 
 connectDB (); // Connect to MongoDB
 
@@ -13,7 +13,5 @@ const app = express ();
 app.get ('/', (req, res) => {
   res.send ('API is running...');
 });
-
-app.use ('/api/products', productRoutes);
 
 app.listen (port, () => console.log (`Server is running on port ${port}`));
